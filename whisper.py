2eaa3ccdf5839cbab79fb0d6d09c3cbe870d30c8
rkilchmn <roger@kilchenmann.net>
2025-01-19 14:56:28 +1100

    default model to smallest (distil-whisper/distil-small.en)

--- whisper.py
+++ whisper.py
@@ -139,7 +139,7 @@ def parse_args(argv=None):
         description='OpenedAI Whisper API Server',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('-m', '--model', action='store', default="openai/whisper-large-v2", help="The model to use for transcription. Ex. distil-whisper/distil-medium.en")
+    parser.add_argument('-m', '--model', action='store', default="distil-whisper/distil-small.en", help="The model to use for transcription. Ex. distil-whisper/distil-medium.en")
     parser.add_argument('-d', '--device', action='store', default="AUTO", help="Set the OpenVino device for the model. Ex. CPU or GPU (default: AUTO)")
     parser.add_argument('-t', '--dtype', action='store', default="auto", help="Set the torch data type for processing (float32, float16, bfloat16)")
     parser.add_argument('-P', '--port', action='store', default=8000, type=int, help="Server tcp port")
